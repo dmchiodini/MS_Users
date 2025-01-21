d9cc43891d0bd7d0b073e37fce09ea1f1feb0a7a
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-21 10:51:51 -0300

    Create celebrate middleware for field validation

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,15 +1,18 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import cors from "cors";
+import { errors } from "celebrate";
 import { routes } from "./routes";
 import { AppError } from "@shared/errors/AppError";
+import "@shared/container";
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
--- src/users/http/routes/users.routes.ts
+++ src/users/http/routes/users.routes.ts
@@ -1,17 +1,83 @@
-import { User } from "@users/entities/User";
-import { UserRepository } from "@users/repository/UserRepository";
-import { createUserController } from "@users/useCases/createUser";
-import { getUsersController } from "@users/useCases/getUsers";
 import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
+import { container } from "tsyringe";
+import { CreateUserController } from "@users/useCases/createUser/CreateUserController";
+import { GetUsersController } from "@users/useCases/getUsers/GetUsersController";
+import { UpdateUserController } from "@users/useCases/updateUser/UpdateUserController";
+import { DeleteUsersController } from "@users/useCases/deleteUser/DeleteUserController";
+import { GetUserByIdController } from "@users/useCases/getUserById/GetUserByIdController";
 
 const usersRouter = Router();
+const createUserController = container.resolve(CreateUserController);
+const getUsersController = container.resolve(GetUsersController);
+const getUserByIdController = container.resolve(GetUserByIdController);
+const updateUserController = container.resolve(UpdateUserController);
+const deleteUsersController = container.resolve(DeleteUsersController);
 
-usersRouter.post("/", (request, response) => {
-  return createUserController.handle(request, response);
-});
+usersRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  (request, response) => {
+    return createUserController.handle(request, response);
+  }
+);
 
-usersRouter.get("/", (request, response) => {
-  return getUsersController.handle(request, response);
-});
+usersRouter.get(
+  "/",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+      limit: Joi.number(),
+    }),
+  }),
+  (request, response) => {
+    return getUsersController.handle(request, response);
+  }
+);
+
+usersRouter.get(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }),
+  }),
+  (request, response) => {
+    return getUserByIdController.handle(request, response);
+  }
+);
+
+usersRouter.patch(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }),
+  }),
+  (request, response) => {
+    return updateUserController.handle(request, response);
+  }
+);
+
+usersRouter.delete(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().uuid().required(),
+    }),
+  }),
+  (request, response) => {
+    return deleteUsersController.handle(request, response);
+  }
+);
 
 export { usersRouter };
